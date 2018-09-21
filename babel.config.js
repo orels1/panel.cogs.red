82e54e6a378f5ef22b7208e63b00c8a971d724f2
orels1 <orel-s1@yandex.ru>
2018-09-21 06:15:55 +0300

    vuetify setup

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,22 @@
 module.exports = {
   presets: [
-    '@vue/app',
+    [
+      '@vue/app',
+      {
+        useBuiltIns: 'entry',
+      },
+    ],
+  ],
+  plugins: [
+    'babel-plugin-transform-class-properties',
+    [
+      'transform-imports',
+      {
+        vuetify: {
+          transform: 'vuetify/es5/components/${member}',
+          preventFullImport: true,
+        },
+      },
+    ],
   ],
 };
