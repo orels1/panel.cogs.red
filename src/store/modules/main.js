6903cc5c4649b7dca6b120ca64cac5bd7c0c8bb8
orels1 <orel-s1@yandex.ru>
2018-12-07 02:54:50 +0300

    userMeta fetching

--- src/store/modules/main.js
+++ src/store/modules/main.js
@@ -1,5 +1,7 @@
 /* eslint-disable no-param-reassign */
 import Vue from 'vue';
+import { authedFetch } from '@/utils';
+import c from '@/constants';
 
 const THEME_SWITCH = 'THEME_SWITCH';
 const LOGIN = 'LOGIN';
@@ -10,6 +12,8 @@ const SET_PROFILE = 'SET_PROFILE';
 const CLEAR_PROFILE = 'CLEAR_PROFILE';
 const SHOW_NOTIFICATION = 'SHOW_NOTIFICATION';
 const HIDE_NOTIFICATION = 'HIDE_NOTIFICATION';
+const SET_USER_META = 'SET_USER_META';
+const CLEAR_USER_META = 'CLEAR_USER_META';
 
 export default {
   state: {
@@ -18,6 +22,10 @@ export default {
     expire: 0,
     authenticated: false,
     profile: {},
+    meta: {
+      admin: false,
+      roles: [],
+    },
     notification: {
       shown: false,
       color: undefined,
@@ -30,6 +38,7 @@ export default {
     expire: state => state.expire,
     authenticated: state => state.authenticated,
     profile: state => state.profile,
+    meta: state => state.meta,
     notification: state => state.notification,
   },
   mutations: {
@@ -64,6 +73,13 @@ export default {
     },
     [HIDE_NOTIFICATION]: (state) => {
       state.notification.shown = false;
+      state.notification.message = '';
+    },
+    [SET_USER_META]: (state, payload) => {
+      state.meta = { ...state.meta, ...payload };
+    },
+    [CLEAR_USER_META]: (state) => {
+      state.meta = {};
     },
   },
   actions: {
@@ -83,6 +99,23 @@ export default {
     },
     authenticate({ commit }) {
       commit(AUTHENTICATE);
+    },
+    async getUserMeta({ commit, rootGetters }) {
+      try {
+        const resp = await authedFetch(`${c.PANEL}/meta`, rootGetters);
+        const json = await resp.json();
+
+        if (!resp.ok) throw new Error(json.error);
+        commit(SET_USER_META, json);
+      } catch (e) {
+        console.error(e);
+        return e.message;
+      }
+      return null;
+    },
+    clearUser({ commit }) {
+      commit(CLEAR_PROFILE);
+      commit(CLEAR_USER_META);
     },
     notify: {
       root: true,
